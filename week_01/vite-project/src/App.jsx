616d5b1bea6397255458aacd5dc207f14c569aa8
mehedimrm22 <mehedi.mrm@gmail.com>
2024-11-04 14:54:30 +0200

    login and logout button added

--- week_01/vite-project/src/App.jsx
+++ week_01/vite-project/src/App.jsx
@@ -5,19 +5,40 @@ import personsData from "./data/personsData.js";
 import { useState } from "react";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLoginClick = () => {
+    setIsLoggedIn(!isLoggedIn);
+  };
+
   return (
     <div>
-      <header>Demo Practicing React App</header>
-      <main>
-        {personsData.map((person, index) => (
-          <Card
-            firstName={person.firstName}
-            title={person.title}
-            age={person.age}
-            sports={person.sports}
-          />
-        ))}
-      </main>
+      {isLoggedIn ? (
+        <>
+          <header>
+            <h1>Demo React App Practicing</h1>
+          </header>
+          <main>
+            <button onClick={handleLoginClick}>Log Out</button>
+            <div className="cards">
+              {personsData.map((person, index) => (
+                <Card
+                  key={person.id}
+                  firstName={person.firstName}
+                  title={person.title}
+                  age={person.age}
+                  sports={person.sports}
+                />
+              ))}
+            </div>
+          </main>
+        </>
+      ) : (
+        <div>
+          <p>Please log in to see the list</p>
+          <button onClick={handleLoginClick}>Log In</button>
+        </div>
+      )}
     </div>
   );
 }
--- week_01/vite-project/src/data/personsData.js
+++ week_01/vite-project/src/data/personsData.js
@@ -1,29 +1,34 @@
 const persons = [
     {
+        "id": 1,
         "firstName": "Mehedi",
         "title": "Developer",
         "age": 26,
         "sports": "Cricket",
     },
     {
+        "id": 2,
         "firstName": "Mahmudur",
         "title": "CEO",
         "age": 28,
         "sports": "Cricket",
     },
     {
+        "id": 3,
         "firstName": "John",
         "title": "Developer",
         "age": 22,
         "sports": "Football",
     },
     {
+        "id": 4,
         "firstName": "Karin",
         "title": "Designer",
         "age": 20,
         "sports": "Badminton",
     },
     {
+        "id": 5,
         "firstName": "Sara",
         "title": "Designer",
         "age": 25,

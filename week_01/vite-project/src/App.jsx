677a6ba12e6726e72513a14edc1bac5bce874ba2
mehedimrm22 <mehedi.mrm@gmail.com>
2024-11-04 13:15:16 +0200

    demo practice with map()

--- week_01/vite-project/src/App.jsx
+++ week_01/vite-project/src/App.jsx
@@ -1,36 +1,22 @@
 import "./App.css";
 import Card from "./Card.jsx";
+import personsData from "./data/personsData.js";
 
 import { useState } from "react";
 
 function App() {
-  const [person, setPerson] = useState({
-    firstName: "Mehedi",
-    title: "Developer",
-    age: 26,
-    sports: "Cricket",
-  });
-
   return (
     <div>
-      <header>Logo</header>
+      <header>Demo Practicing React App</header>
       <main>
-        <Card
-          firstName={person.firstName}
-          title={person.title}
-          age={person.age}
-          sports={person.sports}
-        />
-        <Card
-          firstName="Mahmudur"
-          title="CEO"
-          age="26"
-          sports={person.sports}
-        />
-        <Card firstName="John" title="Developer" age="25" />
-        <Card firstName="Karin" title="Designer" age="45" />
-        <Card firstName="Belayet" title="Developer" age="22" />
-        <Card firstName="Sara" title="Developer" age="25" />
+        {personsData.map((person, index) => (
+          <Card
+            firstName={person.firstName}
+            title={person.title}
+            age={person.age}
+            sports={person.sports}
+          />
+        ))}
       </main>
     </div>
   );
---
+++ week_01/vite-project/src/data/personsData.js
@@ -0,0 +1,34 @@
+const persons = [
+    {
+        "firstName": "Mehedi",
+        "title": "Developer",
+        "age": 26,
+        "sports": "Cricket",
+    },
+    {
+        "firstName": "Mahmudur",
+        "title": "CEO",
+        "age": 28,
+        "sports": "Cricket",
+    },
+    {
+        "firstName": "John",
+        "title": "Developer",
+        "age": 22,
+        "sports": "Football",
+    },
+    {
+        "firstName": "Karin",
+        "title": "Designer",
+        "age": 20,
+        "sports": "Badminton",
+    },
+    {
+        "firstName": "Sara",
+        "title": "Designer",
+        "age": 25,
+        "sports": "Cricket",
+    }
+]
+
+export default persons;

596a1e9ac508baf0cecf676a5f4c11e016606568
mehedimrm22 <mehedi.mrm@gmail.com>
2024-11-20 10:37:05 +0200

    list added

--- week_04/src/pages/List.jsx
+++ week_04/src/pages/List.jsx
@@ -1,5 +1,27 @@
+import personsData from "../data/personsData";
+import Card from "../components/Card";
+
 const List = () => {
-  return <div>I am the list</div>;
+  return (
+    <div>
+      <main>
+        <>
+          <h2>Employees</h2>
+          <div className="list">
+            {personsData.map((person) => (
+              <Card
+                key={person.id}
+                firstName={person.firstName}
+                title={person.title}
+                age={person.age}
+                animal={person.animal}
+              />
+            ))}
+          </div>
+        </>
+      </main>
+    </div>
+  );
 };
 
 export default List;

d3a9518f201f5d7b434f90d7afdb15024b2b4547
mehedimrm22 <mehedi.mrm@gmail.com>
2025-01-08 23:18:00 +0200

    db link

--- week_04/src/pages/List.jsx
+++ week_04/src/pages/List.jsx
@@ -1,9 +1,21 @@
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import personsData from "../data/personsData";
 import Card from "../components/Card";
 
 const List = () => {
   const navigate = useNavigate();
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/posts")
+      .then((response) => response.json())
+      .then((data) => {
+        setPosts(data);
+        setIsLoading(false);
+      });
+  }, []);
 
   const handleNavigate = (id) => {
     navigate(`${id}`);
@@ -15,16 +27,20 @@ const List = () => {
         <>
           <h2>Employees</h2>
           <div className="list">
-            {personsData.map((person) => (
-              <Card
-                key={person.id}
-                firstName={person.firstName}
-                title={person.title}
-                age={person.age}
-                animal={person.animal}
-                onClick={() => handleNavigate(person.id)}
-              />
-            ))}
+            {isLoading ? (
+              <p>Loading...</p>
+            ) : (
+              posts.map((person) => (
+                <Card
+                  key={person.id}
+                  firstName={person.firstName}
+                  title={person.title}
+                  age={person.age}
+                  animal={person.animal}
+                  onClick={() => handleNavigate(person.id)}
+                />
+              ))
+            )}
           </div>
         </>
       </main>
